941b7e9325e464f6a239aeafe5da579a4d5acc0d
KarenNgala <ngalalalu@gmail.com>
2020-05-18 01:04:52 +0300

    Modified js and html code to work on order form first. When no data is entered, error messages display correctly and order total is NaN; but when data is entered, form refreshes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 //business interface logic
+var priceSize, priceCrust, priceTopping;
+
 function Pizza(pizza, size, crust, toppings) {
     this.pizza = pizza;
     this.size = size;
@@ -6,82 +8,72 @@ function Pizza(pizza, size, crust, toppings) {
     this.toppings = toppings;
 }
 
-//calculating price of pizza
-var pricePizza, priceCrust;
-var priceDelivery = 100;
-if (pizzaSize == "large") {
-    pricePizza = 1000;
-} else if (pizzaSize == "medium") {
-    pricePizza = 800;
-} else if (pizzaSize == 'small') {
-    pricePizza = 550;
-} else {
-    pricePizza = 0;
-};
-
-if (pizzaCrust == "crispy") {
-    priceCrust = 100;
-} else if (pizzaCrust == "stuffed") {
-    priceCrust = 200;
-} else if (pizzaCrust == 'gluten') {
-    priceCrust = 150;
-} else {
-    priceCrust = 0;
-};
-
-function priceTopping() {
-    var input = document.getElementsByName("toppings");
-    var total1 = 0;
-    var total = 0;
-    for (var i = 0; i < input.length; i++) {
-        if (input[i].checked) {
-            total1 += parseFloat(input[i].value);
-        }
-        if (pizzaSize == 'large') {
-            total = total1 * 3;
-        } else if (pizzaSize == 'medium') {
-            total = total1 * 2;
-        } else if (pizzaSize == 'small') {
-            total = total1;
-        };
+//calculate pizza price
+var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
+    switch (pizzaSize) {
+        case "0":
+            priceSize = 0;
+            break;
+        case "large":
+            priceSize = 1000;
+            break;
+        case "medium":
+            priceSize = 800;
+            break;
+        case "small":
+            priceSize = 550;
+        default:
+            alert("Please select a pizza size");
     }
-    return total;
+
+    switch (pizzaCrust) {
+        case "0":
+            priceCrust = 0;
+            break;
+        case "crispy":
+            priceCrust = 100;
+            break;
+        case "stuffed":
+            priceCrust = 200;
+            break;
+        case "gluten":
+            priceCrust = 150;
+        default:
+            alert("Please choose a crust");
+    }
+
+    if (pizzaSize == 'large') {
+        priceTopping = pizzaTopping * 3;
+    } else if (pizzaSize == 'medium') {
+        priceTopping = pizzaTopping * 2;
+    } else if (pizzaSize == 'small') {
+        priceTopping = pizzaTopping;
+    }
+
+    return priceCrust, priceSize, priceTopping;
 }
-
-//validate 
-if (pricePizza == '0' | priceCrust == '0') {
-    alert("Please select a pizza and crust");
-}
-
-var pizzaTotal = pricePizza + priceCrust + priceTopping();
-
-
 
 // user interface logic
 $(document).ready(function() {
     $("#order-online").click(function() {
         $(".view-two").show();
         $(".view-one").hide();
-
-        $("#continue").click(function(event) {
-            event.preventDefault();
-            $(".order-total").show();
-            $(".view-two").hide();
-        });
-    });
-    //continue button
-    $("form#form1").submit(function(event) {
-        event.preventDefault();
-
-        //get values from form
+    })
+    $("#continue").click(function() {
+        //get form values
         let pizzaName = $("#pizza option:selected").val();
         let pizzaSize = $("#size option:selected").val();
         let pizzaCrust = $("#crust option:selected").val();
-        var pizzaTopping = [];
-        $.each($("input[name='toppings']:checked"), function() {
-            pizzaTopping.push($(this).text());
+        var pizzaTopping = parseInt([]);
+        $("input:checkbox[name=toppings]:checked").each(function() {
+            pizzaTopping.push($(this).val());
         });
-        console.log(pizzaTopping.join(", "));
 
-    });
-});
+        price(pizzaSize, pizzaCrust, pizzaTopping);
+        var total = priceSize + priceCrust + priceTopping;
+        alert("Your order total is: " + total);
+
+        var order = new Pizza(pizzaName, pizzaSize, pizzaCrust, pizzaTopping)
+
+    })
+})
